25df1023f589524c71bd73d855b86061817f4d01
tiagodc <tdc.florestal@gmail.com>
2024-07-31 00:54:41 -0400

    lastools setup updated

--- setup.py
+++ setup.py
@@ -210,11 +210,11 @@ def addLasDriver(extModules, cxxFlags):
 
         lasModule = Extension(name='pylidar.lidarformats._las',
                 sources=['src/las.cpp', 'src/pylidar.c'],
-                include_dirs=[os.path.join(lastoolsRoot, 'include')],
+                include_dirs=[os.path.join(lastoolsRoot, 'LASlib','inc'), os.path.join(lastoolsRoot, 'LASzip','src'), os.path.join(lastoolsRoot, 'LASzip','dll')],
                 extra_compile_args=cxxFlags,
                 define_macros = [NUMPY_MACROS],
                 libraries=['las'],
-                library_dirs=[os.path.join(lastoolsRoot, 'lib')])
+                library_dirs=[os.path.join(lastoolsRoot, 'LASlib', 'lib')])
                 
         extModules.append(lasModule)
     else:
